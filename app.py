f0f1ec874360511f82cbf93e1eae8ac7d6c6a4ef
Saksham Gupta <saksham812gupta@gmail.com>
2026-09-15 00:04:43 +0530

    Fix Render port configuration

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def predict_datapoint():
         )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
