b6737d785041de4f16ea0f40a162c01c1fcb8eb8
Saksham Gupta <saksham812gupta@gmail.com>
2026-09-14 23:21:52 +0530

    Add Docker deployment and Github Actions

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 import dill
 
 from sklearn.metrics import r2_score
@@ -12,7 +11,6 @@ from src.exception import CustomException
 def save_object(file_path, obj):
 
     try:
-
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(
@@ -29,6 +27,25 @@ def save_object(file_path, obj):
                 obj,
                 file_obj
             )
+
+    except Exception as e:
+
+        raise CustomException(
+            e,
+            sys
+        )
+
+
+def load_object(file_path):
+
+    try:
+
+        with open(
+            file_path,
+            "rb"
+        ) as file_obj:
+
+            return dill.load(file_obj)
 
     except Exception as e:
 
